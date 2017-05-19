b3221fd53300ca0a22ff6d6b42012fdc2dd6bedd
Donma Stark <Donma Stark>
2017-05-19 16:03:14 +0800

    加入專案檔案。

---
+++ GetHwndAndGetSourceFromIE/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace GetHwndAndGetSourceFromIE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtHwnd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(556, 688);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(335, 72);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "GetSource";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(16, 688);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(529, 72);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "List All IE hWnd";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(16, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(875, 266);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.Location = new System.Drawing.Point(16, 324);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(875, 36);
+            this.txtTitle.TabIndex = 7;
+            // 
+            // txtHwnd
+            // 
+            this.txtHwnd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHwnd.Location = new System.Drawing.Point(16, 410);
+            this.txtHwnd.Name = "txtHwnd";
+            this.txtHwnd.ReadOnly = true;
+            this.txtHwnd.Size = new System.Drawing.Size(875, 36);
+            this.txtHwnd.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 383);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "hWnd:";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSource.Location = new System.Drawing.Point(16, 490);
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(875, 185);
+            this.txtSource.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 463);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Source:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 833);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtHwnd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtHwnd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ GetHwndAndGetSourceFromIE/Form1.cs
@@ -0,0 +1,162 @@
+﻿using mshtml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetHwndAndGetSourceFromIE
+{
+    public partial class Form1 : Form
+    {
+        #region API CALLS
+
+        [DllImport("user32.dll", EntryPoint = "GetClassNameA")]
+        public static extern int GetClassName(IntPtr hwnd, StringBuilder lpClassName, int nMaxCount);
+
+        /*delegate to handle EnumChildWindows*/
+        public delegate int EnumProc(IntPtr hWnd, ref IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int EnumChildWindows(IntPtr hWndParent, EnumProc lpEnumFunc, ref IntPtr lParam);
+        [DllImport("user32.dll", EntryPoint = "RegisterWindowMessageA")]
+        public static extern int RegisterWindowMessage(string lpString);
+        [DllImport("user32.dll", EntryPoint = "SendMessageTimeoutA")]
+        public static extern int SendMessageTimeout(IntPtr hwnd, int msg, int wParam, int lParam, int fuFlags, int uTimeout, out int lpdwResult);
+        [DllImport("OLEACC.dll")]
+        public static extern int ObjectFromLresult(int lResult, ref Guid riid, int wParam, ref IHTMLDocument2 ppvObject);
+        public const int SMTO_ABORTIFHUNG = 0x2;
+        public Guid IID_IHTMLDocument = new Guid("626FC520-A41E-11CF-A731-00A0C9082637");
+
+
+        [DllImport("user32.dll")]
+        static extern int EnumWindows(CallbackDef callback, int lParam);
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(int hWnd, StringBuilder text, int count);
+
+        delegate bool CallbackDef(int hWnd, int lParam);
+
+        #endregion
+
+        public IHTMLDocument2 document;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool ShowIEWindowHandler(int hWnd, int lParam)
+        {
+            string mystring;
+
+            StringBuilder text = new StringBuilder(255);
+            GetWindowText(hWnd, text, 255);
+
+            mystring = text.ToString();
+            if (mystring.Contains("Internet Explorer"))
+            {
+                listBox1.Items.Insert(0, text + "," + hWnd);
+                
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+
+            listBox1.Items.Clear();
+            CallbackDef callback = new CallbackDef(ShowIEWindowHandler);
+            EnumWindows(callback, 0);
+
+        }
+
+        private IHTMLDocument2 documentFromDOM(IntPtr hWnd)
+        {
+
+
+            int lngMsg = 0;
+            int lRes;
+
+            EnumProc proc = new EnumProc(EnumWindows);
+            EnumChildWindows(hWnd, proc, ref hWnd);
+            if (!hWnd.Equals(IntPtr.Zero))
+            {
+                lngMsg = RegisterWindowMessage("WM_HTML_GETOBJECT");
+                if (lngMsg != 0)
+                {
+                    SendMessageTimeout(hWnd, lngMsg, 0, 0, SMTO_ABORTIFHUNG, 1000, out lRes);
+                    if (!(bool)(lRes == 0))
+                    {
+                        int hr = ObjectFromLresult(lRes, ref IID_IHTMLDocument, 0, ref document);
+                        if ((bool)(document == null))
+                        {
+                            MessageBox.Show("CANNOT FOUND IHTMLDocument");
+
+                        }
+                    }
+
+                }
+
+            }
+
+            return document;
+        }
+
+        private int EnumWindows(IntPtr hWnd, ref IntPtr lParam)
+        {
+            int retVal = 1;
+            StringBuilder classname = new StringBuilder(128);
+            GetClassName(hWnd, classname, classname.Capacity);
+            if ((bool)(string.Compare(classname.ToString(), "Internet Explorer_Server") == 0))
+            {
+                lParam = hWnd;
+                retVal = 0;
+            }
+            return retVal;
+        }
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtHwnd.Text = listBox1.Text.Split(',')[1];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem == null)
+            {
+                MessageBox.Show("Choose hWnd first.");
+                return;
+            }
+            txtSource.Text = "";
+            txtTitle.Text = "";
+
+            var document = documentFromDOM(new IntPtr(int.Parse(txtHwnd.Text)));
+
+            if (document != null)
+            {
+                txtTitle.Text = document.title;
+                txtSource.Text = document.body.innerHTML;
+            }
+            else {
+                MessageBox.Show("Sorry I cant fetch it.");
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
